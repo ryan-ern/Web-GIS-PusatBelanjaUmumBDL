4c9bf0ed1c42738f9bcdb297c36e8696afb9fbf0
ryan-ern <ryanernanda100902@gmail.com>
2023-03-26 16:24:25 +0700

    first commit

---
+++ data/Pusat_Belanja_Umum_BDL_1.js
@@ -0,0 +1,369 @@
+var json_Pusat_Belanja_Umum_BDL_1 = {
+  type: "FeatureCollection",
+  name: "Pusat_Belanja_Umum_BDL_1",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Mall Boemi Kedaton",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\mbk.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.259583649760074, -5.382053185505258],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Transmart Carrefour",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\transmart.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.282248297082333, -5.383380520986569],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Mitra10 Antasari",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\mitra10.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.285834644217445, -5.400250702247345],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Simpur Center",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\simpur.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.257430508264463, -5.412985984460144],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Mall Kartini",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\mkartini.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.253297543164521, -5.416649236419651],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Tamin",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\tamin.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.247458063442693, -5.410464167676328],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Ciplaz Lampung",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\ciplaz.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.23801890444021, -5.371069373290707],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Untung",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\untung.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.256310608043819, -5.36252110931346],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Tempel",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\tempel.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.294120572635563, -5.383015504018042],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Cosmo Palapa",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\cosmo.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.246498148967831, -5.420339011233025],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Tugu",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\tugu.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.266256388574661, -5.409216528116976],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Bambu Kuning",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\bambu.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.256470593789643, -5.410649986113961],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Senen",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\senen.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.270069382182996, -5.399341503020429],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Gintung",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\gintung.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.255750657933504, -5.40812815959196],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Way Halim",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\tempel.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.271429261022291, -5.384196830868594],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Lampung City Mall",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\city.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.280228477041533, -5.443605308717108],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Gudang Lelang",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\gudang.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.268976146253308, -5.451913534699295],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Perumnas W.Hal",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\perumnas.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.275522229685791, -5.377161929255045],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Ramayana",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\ramayana.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.258757056740038, -5.410742895311378],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Buah",
+        foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\buah.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.274465657156085, -5.418587034635458],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { nama_bel: "PB Swalayan", foto: "assets/pb.png" },
+      geometry: {
+        type: "Point",
+        coordinates: [105.304033856061537, -5.360665056649973],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Chandra Way Halim",
+        foto: "assets/chandra_whp.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.271895886114237, -5.379046552531888],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Tempel Rajabsa",
+        foto: "assets/tempel_rjbs.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.239328384814726, -5.353237500665511],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Tempel WKandis",
+        foto: "assets/tempel_kandis.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.291363748637394, -5.352646807534319],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Surya Sukabumi",
+        foto: "assets/surya_sukabumi.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.298239917158142, -5.395752661964596],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Fitrinofane Sukabumi",
+        foto: "assets/fitrinofane_skbm.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.297470819015544, -5.3955290936519],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { nama_bel: "Pasar Koga", foto: "assets/koga.png" },
+      geometry: {
+        type: "Point",
+        coordinates: [105.261702339201292, -5.392026650926805],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { nama_bel: "Cosmo Kedaton", foto: "assets/cosmo-kdtn.png" },
+      geometry: {
+        type: "Point",
+        coordinates: [105.261669008837586, -5.388568985135896],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { nama_bel: "Pasar Cimeng", foto: "assets/cimeng.png" },
+      geometry: {
+        type: "Point",
+        coordinates: [105.257016090063814, -5.44919141280025],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Ikan Sukaraja",
+        foto: "assets/ikan_sukaraja.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.288153315840745, -5.443398202764061],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Gedung Kuning",
+        foto: "assets/gedung_kuning.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.304559354117572, -5.360168179244289],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Chandra Kemiling",
+        foto: "assets/chandra_kemiling.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.213213326077451, -5.397302290898472],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Warna Asri",
+        foto: "assets/warna_asri.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.201347716597041, -5.402571663293636],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        nama_bel: "Pasar Panjang",
+        foto: "assets/Pasar_panjang.png",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [105.322950215553277, -5.472277014869539],
+      },
+    },
+  ],
+};
