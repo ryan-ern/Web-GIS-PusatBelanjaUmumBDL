d8d7be193f118941c584bf91ddd9ab206cdac2eb
ryan-ern <ryanernanda100902@gmail.com>
2023-03-26 21:40:21 +0700

    update

--- data/Pusat_Belanja_Umum_BDL_1.js
+++ data/Pusat_Belanja_Umum_BDL_1.js
@@ -8,6 +8,8 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Mall Boemi Kedaton",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\mbk.png",
+        Keterangan:
+          "Luas tanah 30.000 m2, luas bangunan 55.000 m2, jumlah lantai 4",
       },
       geometry: {
         type: "Point",
@@ -19,6 +21,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Transmart Carrefour",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\transmart.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -30,6 +33,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Mitra10 Antasari",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\mitra10.png",
+        Keterangan: "Luas tanah 10.000 m2, luas bangunan 12.000 m2",
       },
       geometry: {
         type: "Point",
@@ -41,6 +45,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Simpur Center",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\simpur.png",
+        Keterangan: "Jumlah lantai 4",
       },
       geometry: {
         type: "Point",
@@ -52,6 +57,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Mall Kartini",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\mkartini.png",
+        Keterangan: "Jumlah lantai 4",
       },
       geometry: {
         type: "Point",
@@ -63,6 +69,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Tamin",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\tamin.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -74,6 +81,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Ciplaz Lampung",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\ciplaz.png",
+        Keterangan: "Luas tanah 14.158 m2, jumlah lantai 3",
       },
       geometry: {
         type: "Point",
@@ -85,6 +93,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Untung",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\untung.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -96,6 +105,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Tempel",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\tempel.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -107,6 +117,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Cosmo Palapa",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\cosmo.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -118,6 +129,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Tugu",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\tugu.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -129,6 +141,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Bambu Kuning",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\bambu.png",
+        Keterangan: "Luas tanah 15.061 m2, jumlah lantai 3",
       },
       geometry: {
         type: "Point",
@@ -140,6 +153,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Senen",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\senen.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -151,6 +165,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Gintung",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\gintung.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -162,6 +177,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Way Halim",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\tempel.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -173,6 +189,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Lampung City Mall",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\city.png",
+        Keterangan: "Luas tanah 26.000 m2, jumlah lantai 6",
       },
       geometry: {
         type: "Point",
@@ -184,6 +201,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Gudang Lelang",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\gudang.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -195,6 +213,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Perumnas W.Hal",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\perumnas.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -206,6 +225,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Ramayana",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\ramayana.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -217,6 +237,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Buah",
         foto: "D:\\Kuliah\\Tugas\\SIG\\Web\\assets\\buah.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -225,7 +246,11 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
     },
     {
       type: "Feature",
-      properties: { nama_bel: "PB Swalayan", foto: "assets/pb.png" },
+      properties: {
+        nama_bel: "PB Swalayan",
+        foto: "assets/pb.png",
+        Keterangan: "Tidak ada",
+      },
       geometry: {
         type: "Point",
         coordinates: [105.304033856061537, -5.360665056649973],
@@ -236,6 +261,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Chandra Way Halim",
         foto: "assets/chandra_whp.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -247,6 +273,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Tempel Rajabsa",
         foto: "assets/tempel_rjbs.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -258,6 +285,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Tempel WKandis",
         foto: "assets/tempel_kandis.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -269,6 +297,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Surya Sukabumi",
         foto: "assets/surya_sukabumi.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -280,6 +309,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Fitrinofane Sukabumi",
         foto: "assets/fitrinofane_skbm.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -288,7 +318,11 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
     },
     {
       type: "Feature",
-      properties: { nama_bel: "Pasar Koga", foto: "assets/koga.png" },
+      properties: {
+        nama_bel: "Pasar Koga",
+        foto: "assets/koga.png",
+        Keterangan: "Tidak ada",
+      },
       geometry: {
         type: "Point",
         coordinates: [105.261702339201292, -5.392026650926805],
@@ -296,7 +330,11 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
     },
     {
       type: "Feature",
-      properties: { nama_bel: "Cosmo Kedaton", foto: "assets/cosmo-kdtn.png" },
+      properties: {
+        nama_bel: "Cosmo Kedaton",
+        foto: "assets/cosmo-kdtn.png",
+        Keterangan: "Tidak ada",
+      },
       geometry: {
         type: "Point",
         coordinates: [105.261669008837586, -5.388568985135896],
@@ -304,7 +342,11 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
     },
     {
       type: "Feature",
-      properties: { nama_bel: "Pasar Cimeng", foto: "assets/cimeng.png" },
+      properties: {
+        nama_bel: "Pasar Cimeng",
+        foto: "assets/cimeng.png",
+        Keterangan: "Tidak ada",
+      },
       geometry: {
         type: "Point",
         coordinates: [105.257016090063814, -5.44919141280025],
@@ -315,6 +357,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Ikan Sukaraja",
         foto: "assets/ikan_sukaraja.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -326,6 +369,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Gedung Kuning",
         foto: "assets/gedung_kuning.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -337,6 +381,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Chandra Kemiling",
         foto: "assets/chandra_kemiling.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -348,6 +393,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Warna Asri",
         foto: "assets/warna_asri.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
@@ -359,6 +405,7 @@ var json_Pusat_Belanja_Umum_BDL_1 = {
       properties: {
         nama_bel: "Pasar Panjang",
         foto: "assets/Pasar_panjang.png",
+        Keterangan: "Tidak ada",
       },
       geometry: {
         type: "Point",
